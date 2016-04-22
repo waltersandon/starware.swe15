4f80022e90ba542c1e44d36d3e01bf290f5c7740
avenier <andrea.egna@gmail.com>
2016-04-22 19:17:13 +0200

    modificato script di caricamento db

--- src/api/data/Question.js
+++ src/api/data/Question.js
@@ -2,7 +2,7 @@ var _ = require('./Tag');
 var _ = require('./User');
 var mongoose = require('mongoose');
 
-var QuestionSchema = mongoose.model('Question', new mongoose.Schema({
+var Question = mongoose.model('Question', new mongoose.Schema({
     author: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
@@ -19,20 +19,4 @@ var QuestionSchema = mongoose.model('Question', new mongoose.Schema({
     }]
 }));
 
-
-// User model
-var Question = mongoose.model('Question', QuestionSchema);
-/*
-// JSON formatting
-QuestionSchema.options.toJSON = {
-    transform : function(doc, ret, options) {
-        return {
-            id: ret._id,
-            body: ret.fullName,
-            author: { href: "/api/roles/"+ret.role+"/" },
-            tags: { href: "/api/roles/"+ret.role+"/" }
-        };
-    }
-};
-*/
 module.exports = Question;
