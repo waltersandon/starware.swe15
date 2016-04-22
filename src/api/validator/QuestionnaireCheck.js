8dbf8c87916a0bfda2f171dbf4ca8010245e627c
avenier <andrea.egna@gmail.com>
2016-04-22 19:40:56 +0200

    questionnaire check

--- src/api/validator/QuestionnaireCheck.js
+++ src/api/validator/QuestionnaireCheck.js
@@ -5,6 +5,24 @@
  */
 function QuestionnaireCheck() {
 
+    /*Da controllare*/
+    this.checkQuestions = function(questionArray){
+        if (typeof questionArray !== 'undefined' && questionArray.length > 0) {
+            return true;
+        }else{
+            return false;
+        }
+    };
+
+    /*Da controllare*/
+    this.checkTitle = function(n){
+        if(n.length == 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    };
 
 }
 
