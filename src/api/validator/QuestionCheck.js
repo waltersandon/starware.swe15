a7d80fb2aff42b098d35a230e7ef69d63b10906e
alevit33 <alessio.vitella@gmail.com>
2016-04-22 19:40:20 +0200

    validator qml server

--- src/api/validator/QuestionCheck.js
+++ src/api/validator/QuestionCheck.js
@@ -13,14 +13,27 @@ function QuestionCheck() {
             }
             this.type = qml.substring(1,this.i);
             if(this.type == "TF"){
-                return this.checkTF(qml.substring(this.i +1));
+                return this.checkTF(qml.substring(this.i + 1));
+            }
+            else if(this.type == "MultipleChoice"){
+                return this.checkMultipleChoice(qml.substring(this.i + 1));
             }
         }
-
-
+        return false;
 
         this.checkTF = function(qml) {
-            return true;
+            this.i = 0;
+            while(qml.charAt(this.i) != "["){
+                this.i++
+            }
+            if((qml.charAt(this.i + 1) == "T" || qml.charAt(this.i + 1) == "F" )
+                && qml.charAt(this.i + 2) == "]"
+                && qml.length == 3) {
+                return true;
+            }
+            else{
+                return false;
+            }
         };
 
         this.checkMultipleChoice = function(qml) {
