c98e9a8474073598c0af122dc7733ae4200b1989
alevit33 <alessio.vitella@gmail.com>
2016-04-26 16:21:54 +0200

    Cambiamento dipendenze app

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var fs = require('fs');
 
 var App = require('./api/app/App');
+var Loader = require('./api/middleware/Loader');
 
 var a = new App();
+var l = new Loader(a.config());
 a.start();
