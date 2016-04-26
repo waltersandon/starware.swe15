9d45114745298558539e40c2fc00dc64fbfcdb04
avenier <andrea.egna@gmail.com>
2016-04-26 16:30:27 +0200

    fix bootstrap.js

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -34,15 +34,15 @@ mongoose.connect(config.dbUri, function() {
 		var tag2 = 	new Tag({name: 'Informatica', description: 'Scienza che si occupa dello studio dei computer'});
 		var tag3 = 	new Tag({name: 'Italiano', description: 'Scienza che si occupa dello studio della lingua e grammatica italiana'});
 		var tag4 =  new Tag({name: 'SWE', description: 'Scienza che si occupa dello studio della qualità di un SW',parent: tag2._id});
-		
+
 		var usr1 = new User ({fullName:'Mario Rossi', password: 'password', role: student._id, userName: "mrossi" });
 		var usr2 = new User ({fullName:'Carlo Bianchi', password: 'password', role: teacher._id, userName: "cbianchi" });
 		var usr3 = new User ({fullName:'Amilcare Verdi', password: 'password', role: admin._id, userName: "averdi" });
 		var usr4 = new User ({fullName:'Mariuccia Pastafrolla', password: 'password', role: superAdmin._id, userName: "mpastafrolla" });
 
-		var question1 = new Question ({author: usr2._id, body: 'domanda Matematica', tags: [tag1._id,tag2._id]});
-		var question2 = new Question ({author: usr3._id, body: 'domanda Informatica', tags: [tag2._id]});
-		var question3 = new Question ({author: usr4._id, body: 'domanda Italiano', tags: [tag3._id]});
+		var question1 = new Question ({author: usr2._id, body: "<TF>\nRoma è la capitale d’**Italia**?\n[T]", tags: [tag2._id,tag1._id]});
+ 		var question2 = new Question ({author: usr3._id, body: "<TF>\nRoma è la capitale d’**Italia**?\n[T]", tags: [tag2._id]});
+		var question3 = new Question ({author: usr4._id, body: "<TF>\nRoma è la capitale d’**Italia**?\n[T]", tags: [tag3._id]});
 
 		var questionnaire1 = new Questionnaire({author: usr2._id, questions: [question1._id,question2._id,question3._id], tags: [tag1._id,tag2._id,tag3._id], title: "Quiz 1"});
 
@@ -65,14 +65,16 @@ mongoose.connect(config.dbUri, function() {
 			question2.save(),
 			question3.save(),
 			questionnaire1.save()
-		]).then(function() {});
-		
-		mongoose.disconnect();
-		console.log('Aggiunti ruoli base');
-		console.log('Aggiunti agromenti base');
-		console.log('Aggiunti utenti base');
-		console.log('Aggiunte domande base');
-		console.log('Aggiunto questionario base');
+
+		]).then(function() {
+			mongoose.disconnect();
+			console.log('Aggiunti ruoli base');
+			console.log('Aggiunti agromenti base');
+			console.log('Aggiunti utenti base');
+			console.log('Aggiunte domande base');
+			console.log('Aggiunto questionario base');
+		});
+
 	});
 
 });
