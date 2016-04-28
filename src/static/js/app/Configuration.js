256007944377489eb5d19ccb3f3c81d680d7d1d9
alevit33 <alessio.vitella@gmail.com>
2016-04-28 10:02:38 +0200

    correzioni login

--- src/static/js/app/Configuration.js
+++ src/static/js/app/Configuration.js
@@ -1,5 +1,6 @@
 $(function () {
     angular.module('ConfigurationModule', []).service('app.Configuration', [function () {
-            this.remote = 'https://quizzipedia-starware.rhcloud.com/';
+            //this.remote = 'https://quizzipedia-starware.rhcloud.com/';
+            this.remote="";
         }]);
 });
