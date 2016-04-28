ef54b4ae54b85080a55d412373bf46423ff02771
lordnikolai <nicola.decao@studenti.unipd.it>
2016-04-28 10:02:48 +0200

    try fix

--- src/static/js/controller/public/LogIn.js
+++ src/static/js/controller/public/LogIn.js
@@ -2,12 +2,14 @@ $(function () {
     angular.module('app.App').controller('controller.public.LogIn', ['$cookies', 'model.data.CurrentUser', '$location', '$scope', '$rootScope', 'model.service.SessionService', 'model.service.UserService', function ($cookies, CurrentUser, $location, $scope, $rootScope, SessionService, UserService) {
             $scope.submit = function () {
                 if (SessionService.login($scope.password, $scope.userName)) {
+                    alert('ok');
                     var me = UserService.getMe();
                     $rootScope.me = me;
                     $cookies.putObject('me', me);
                     $location.url('#/user');
+                    alert('ok2');
                 } else {
-
+                    alert('no ok');
                 }
             };
         }]);
