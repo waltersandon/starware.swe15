426a75e0f17f31943b04e9530f280cc4784666a5
alevit33 <alessio.vitella@gmail.com>
2016-04-28 14:46:52 +0200

    Tolto _v nel toJSON di role

--- src/api/data/Role.js
+++ src/api/data/Role.js
@@ -7,5 +7,15 @@ var RoleSchema = new mongoose.Schema({
     }
 });
 
+/* Metodo che viene chiamato quando è necessario restituire
+ * una ur ruolo in formato JSON all'esterno
+ */
+TagSchema.options.toJSON = {
+    transform: function(doc, ret, options) {
+        delete ret.__v;
+        return ret;
+    }
+};
+
 var Role = mongoose.model('Role', RoleSchema);
 module.exports = Role;
