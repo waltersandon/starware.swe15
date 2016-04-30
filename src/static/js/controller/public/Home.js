d103a9be44135a43a70f0369671f0fed5ebccf0b
lordnikolai <nicola.decao@studenti.unipd.it>
2016-04-30 15:00:39 +0200

    finito login 100%

--- src/static/js/controller/public/Home.js
+++ src/static/js/controller/public/Home.js
@@ -1,7 +1,26 @@
 $(function () {
-    angular.module('app.App').controller('controller.public.Home', ['$location', '$rootScope', '$scope', function ($location, $rootScope, $scope) {
+    angular.module('app.App').controller('controller.public.Home', ['$cookies', '$location', '$rootScope', '$scope', 'model.service.SessionService', 'model.service.UserService', function ($cookies, $location, $rootScope, $scope, SessionService, UserService) {
+            $rootScope.me = true;
             $scope.urlPath = function () {
                 return $location.path().split('/');
             };
+
+            if ($cookies.getObject('me')) {
+                SessionService.login($cookies.getObject('me').password, $cookies.getObject('me').userName, function () {
+                    UserService.getMe($cookies.getObject('me').password, function (me) {
+                        $rootScope.me = $cookies.getObject('me');
+
+                        var now = new Date();
+                        $cookies.putObject('me', me, {expires: new Date(now.getFullYear() + 1, now.getMonth(), now.getDate())});
+                        $location.path('user');
+                    }, function (res) {
+                        $rootScope.me = false;
+                    });
+                }, function (res) {
+                    $rootScope.me = false;
+                });
+            } else {
+                $rootScope.me = false;
+            }
         }]);
 });
--- src/static/js/controller/public/LogIn.js
+++ src/static/js/controller/public/LogIn.js
@@ -1,5 +1,5 @@
 $(function () {
-    angular.module('app.App').controller('controller.public.LogIn', ['util.Check', '$cookies', 'model.data.CurrentUser', 'model.data.Error', '$location', '$scope', '$rootScope', 'model.service.SessionService', 'model.service.UserService', function (Check, $cookies, CurrentUser, Error, $location, $scope, $rootScope, SessionService, UserService) {
+    angular.module('app.App').controller('controller.public.LogIn', ['util.Check', '$cookies', 'model.data.Error', '$location', '$scope', '$rootScope', 'model.service.SessionService', 'model.service.UserService', function (Check, $cookies, Error, $location, $scope, $rootScope, SessionService, UserService) {
             $scope.error = new Error();
             $scope.checkPassword = function () {
                 if ($scope.password) {
@@ -16,15 +16,18 @@ $(function () {
             $scope.submit = function () {
                 SessionService.login(md5($scope.password), $scope.userName, function () {
                     UserService.getMe(md5($scope.password), function (me) {
-                        var now = new Date();
-                        
                         $rootScope.me = me;
-                        $cookies.putObject('me', me, {expires:  new Date(now.getFullYear() + 1, now.getMonth(), now.getDate())});   
-                        $location.path("user");
+
+                        var now = new Date();
+                        $cookies.putObject('me', me, {expires: new Date(now.getFullYear() + 1, now.getMonth(), now.getDate())});
+
+                        $location.path('user');
                     }, function (res) {
+                        $rootScope.me = false;
                         $scope.error = new Error(res.data.message, 'errorGetMe', true, 'alert-danger');
                     });
                 }, function (res) {
+                    $rootScope.me = false;
                     $scope.error = new Error(res.data.message, 'errorLogin', true, 'alert-danger');
                 });
             };
