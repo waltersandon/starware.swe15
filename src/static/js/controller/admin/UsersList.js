a9f244b0f0f0c9eafbf8574ab6ff89838fe460eb
lordnikolai <nicola.decao@studenti.unipd.it>
2016-04-30 21:40:28 +0200

    client cominciato userlist

--- src/static/js/controller/admin/UsersList.js
+++ src/static/js/controller/admin/UsersList.js
@@ -1,5 +1,45 @@
 $(function () {
-    angular.module('app.App').controller('controller.admin.UsersList', ['$location', '$rootScope', '$scope', function ($location, $rootScope, $scope) {
+    angular.module('app.App').controller('controller.admin.UsersList', ['$location', 'model.service.RoleService', '$rootScope', '$scope', 'model.service.UserService', function ($location, RoleService, $rootScope, $scope, UserService) {
+            RoleService.get(null, function (roles) {
+                roles.forEach(function (item) {
+                    switch (item.name) {
+                        case 'student':
+                            item.name = 'Studente';
+                            break;
+                        case 'teacher':
+                            item.name = 'Docente';
+                            break;
+                        case 'admin':
+                            item.name = 'Amministratore';
+                            break;
+                        case 'superadmin':
+                            item.name = 'Proprietario';
+                            break;
+                    }
+                });
+                $scope.roles = roles;
+            }, function (res) {
 
+            });
+            UserService.get(null, null, function (users) {
+                $scope.usersList = users;
+            }, function (res) {
+
+            });
+            $scope.changeUserRole = function (user, role) {
+                console.log(role);
+                UserService.modifyRole(user, role, function () {
+                    console.log('success!');
+                }, function (res) {
+                    
+                });
+            };
+            $scope.deleteUser = function (user) {
+                UserService.delete(user, function () {
+                    $scope.usersList.splice($scope.usersList.indexOf(user), 1);
+                }, function (res) {
+                    
+                });
+            };
         }]);
 });
