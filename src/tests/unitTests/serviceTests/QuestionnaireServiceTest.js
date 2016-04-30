64cb79ccfa3dfc962910c5a480b85f862f770484
Igor Baylyak <igor@MacBook-Air-di-Igor.local>
2016-04-30 21:33:20 +0200

    agiato Test POST /api/questionnaires

--- src/tests/unitTests/serviceTests/QuestionnaireServiceTest.js
+++ src/tests/unitTests/serviceTests/QuestionnaireServiceTest.js
@@ -79,11 +79,111 @@ describe('GET /api/questionnaires/:id', function() {
         });
     });
 });
-describe('POST /api/questionnaires', function() {});
+describe('POST /api/questionnaires', function() {
+    it('impedisce l\'accesso ad un utente non autorizzato', function (done) {
+        login(app, {
+            userName: 'tullio.vardanega',
+            password: 'password.tullio.vardanega'
+        }, function(agent) {
+            var newQuestionnaire ={
+                title: "Quiz Test" ,
+                tags: [{ id: undefined }],
+                questions: [{id: undefined }]
+            };
+            var req = request(app).get('/api/tags');
+            agent.attachCookies(req);
+            req.end(function(err, res) {
+                expect(err).to.not.be.ok;
+                expect(res).to.have.property('status', 200);
+                expect(res.body.length > 0).to.be.equal(true);
+                newQuestionnaire.tags = res.body;
+                var req = request(app).get('/api/questions');
+                agent.attachCookies(req);
+                req.end(function(err, res) {
+                    expect(err).to.not.be.ok;
+                    expect(res).to.have.property('status', 200);
+                    expect(res.body.length > 0).to.be.equal(true);
+                    newQuestionnaire.questions = res.body;
+                    login(app, {
+                        userName: 'mario.rossi',
+                        password: 'password.mario.rossi'
+                    }, function(agent) {
+                        var req = request(app).post('/api/questionnaires');
+                        agent.attachCookies(req);
+                        req.send(newQuestionnaire).end(function(err, res) {
+                            expect(err).to.not.be.ok;
+                            expect(res).to.have.property('status', 401);
+                            done();
+                        });
+                    });
+                });
+            });
+        })
+    });
+    it('permette la creazione del quiz di test  all\'utente autenticato', function (done) {
+        login(app, {
+            userName: 'tullio.vardanega',
+            password: 'password.tullio.vardanega'
+        }, function(agent) {
+            var newQuestionnaire ={
+                title: "Quiz Test" ,
+                tags: [],
+                questions: []
+            };
+            var req = request(app).get('/api/tags');
+            agent.attachCookies(req);
+            req.end(function(err, res) {
+                expect(err).to.not.be.ok;
+                expect(res).to.have.property('status', 200);
+                expect(res.body.length > 0).to.be.equal(true);
+                var tags_id = [];
+                res.body.forEach(function (elem) {
+                        tags_id.push({"id": elem._id} );
+                });
+                //console.log(tags_id);
+                newQuestionnaire.tags = tags_id;
+                var req = request(app).get('/api/questions');
+                agent.attachCookies(req);
+                req.end(function(err, res) {
+                    expect(err).to.not.be.ok;
+                    expect(res).to.have.property('status', 200);
+                    expect(res.body.length > 0).to.be.equal(true);
+                    var questions_id = [];
+                    res.body.forEach(function (elem) {
+                        questions_id.push({"id": ""+elem._id+"" } );
+
+                    });
+                    //console.log(questions_id);
+                    newQuestionnaire.questions = questions_id;
+                    var req = request(app).post('/api/questionnaires');
+                    //console.log(newQuestionnaire);
+                    agent.attachCookies(req);
+                    req.send(newQuestionnaire).end(function(err, res) {
+                            expect(err).to.not.be.ok;
+                            expect(res).to.have.property('status', 200);
+
+                        var req = request(app).get('/api/questionnaires');
+                        agent.attachCookies(req);
+                        req.end(function(err, res) {
+                            expect(res).to.have.property('status', 200);
+
+                            expect(res.body.find(function (questionnaire) {
+                                return questionnaire.title === "Quiz Test";
+                            }).title).to.eql("Quiz Test");
+
+                            done();
+                        });
+                    });
+                });
+            });
+        })
+    });
+});
 describe('PUT /api/questionnaires/:id', function() {});
+/*
 describe('DELETE /api/questionnaires/:id', function() {
 
-    it('impedisce l\'accesso ad un utente non autorizzato', function (done) {
+    /*it('impedisce l\'accesso ad un utente non autorizzato', function (done) {
         login(app, {
             userName: 'mario.rossi',
             password: 'password.mario.rossi'
@@ -92,7 +192,7 @@ describe('DELETE /api/questionnaires/:id', function() {
             agent.attachCookies(req);
             req.end(function(err, res) {
                 var questionnaire = res.body.find(function (questionnnaire) {
-                    return questionnnaire.name === "testTagDopoPut";
+                    return questionnnaire.title === "Quiz Test Dopo Put";
                 });
                 var req = request(app).delete('/api/questionnaires/'+questionnaire._id);
                 agent.attachCookies(req);
@@ -135,11 +235,8 @@ describe('DELETE /api/questionnaires/:id', function() {
         });
     });
 });
-
+*/
 /*
- this.router.get('/questionnaires',auth.requireUser,this.questionnaireService.get);
- this.router.get('/questionnaires/:id',auth.requireUser,this.questionnaireService.getByID);
- this.router.post('/questionnaires',auth.requireTeacher,this.questionnaireService.new);
  this.router.put('/questionnaires/:id',auth.requireTeacher,this.questionnaireService.modify);
  this.router.delete('/questionnaires/:id',auth.requireTeacher,this.questionnaireService.delete);
  */
