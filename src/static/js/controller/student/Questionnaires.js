78177c5cf3a56854af3d3a92df7e9107c998d17d
alevit33 <alessio.vitella@gmail.com>
2016-05-03 00:23:48 +0200

    ricerca questionari e inizio execute questionario

--- src/static/js/controller/student/Questionnaires.js
+++ src/static/js/controller/student/Questionnaires.js
@@ -1,5 +1,5 @@
 $(function () {
-    angular.module('app.App').controller('controller.student.Questionnaires', ['$location', '$rootScope', '$scope', '$q', 'model.service.QuestionnaireService', 'model.service.UserService', 'model.service.TagService', function ($location,  $rootScope, $scope, $q, questionnaireService, UserService, TagService) {
+    angular.module('app.App').controller('controller.student.Questionnaires', ['$location', '$rootScope', '$scope', '$q', 'model.service.QuestionnaireService', 'model.service.UserService', 'model.service.TagService', function ($location,  $rootScope, $scope, $q, QuestionnaireService, UserService, TagService) {
         $scope.titleSearch = '';
         $scope.tagSearch = '';
         $scope.authorSearch = '';
@@ -13,7 +13,7 @@ $(function () {
                 var deferred = $q.defer();
 
                 if($scope.authorSearch) {
-                    UserService.get($scope.authorSearch.split(' '), [], function(aut){
+                    UserService.get($scope.authorSearch.split(' '), null, function(aut){
                         var list = [];
                         aut.forEach(function(value,index){
                             list.push(value.id);
@@ -67,11 +67,33 @@ $(function () {
                 titles = $scope.titleSearch.split(' ');
             }
 
+            //una volta trovati gli id dei tag e degli autori che corrispondono alla ricerca mi sincronizzo e
+            //procedo con la ricerca del questionario
             $q.all([this.authors(), this.tags()]).then(function(result){
-                questionnaireService.get(result[0], result[1], titles, function (quest) {
-                    $scope.questList = quest;
-                }, function () {
-
+                QuestionnaireService.get(result[0], result[1], titles, function (questionnaires) {
+                    var tags = [];
+                    async.each(questionnaires, function (questionnaire, cb) {
+                        async.each(questionnaire.tags, function (tag, cll) {
+                            TagService.getByID(tag, function (tagComplete) {
+                                tags.push(tagComplete.name);
+                                cll();
+                            }, function () {
+                                cll();
+                            });
+                        }, function () {
+                            questionnaire.tags = tags;
+                            UserService.getByID(questionnaire.author, function(aut){
+                                questionnaire.author = aut.fullName;
+                                cb();
+                            }, function(){
+                                cb();
+                            });
+                        });
+                    }, function () {
+                        $scope.questList = questionnaires;
+                    });
+                }, function (err) {
+                    $questList = [];
                 });
             }, function(){
                 //Utenti o tag corrispondenti alla ricerca non trovati
@@ -82,10 +104,8 @@ $(function () {
         };
 
         $scope.beginQuest = function(questID) {
-            $location.path('student/exequestionnaires/'+questID);
+            $location.path('student/questionnaire/'+questID);
         };
-
-        $scope.submit();
 
     }]);
 });
