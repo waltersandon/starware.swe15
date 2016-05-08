3de590216c5f64a27b88422ba2c2a5f132b60777
lordnikolai <nicola.decao@studenti.unipd.it>
2016-05-08 17:24:16 +0200

    injection di confirm ed editor

--- src/static/js/controller/teacher/ManipulateQuestion.js
+++ src/static/js/controller/teacher/ManipulateQuestion.js
@@ -31,14 +31,7 @@ $(function () {
             }
 
             function setEditor() {
-                $scope.editor = new SimpleMDE({
-                    element: document.getElementById('editor'),
-                    previewRender: function (plainText) {
-                        var p = QML.parse(plainText);
-                        return p.status ? p.body + p.answerForm : p.message;
-                    },
-                    toolbar: ['bold', 'italic', '|', 'quote', 'unordered-list', 'ordered-list', '|', 'link', 'image', 'guide', '|', 'preview']
-                });
+                $scope.editor = QML.editor();
                 $scope.editor.value($scope.question.body);
             }
 
