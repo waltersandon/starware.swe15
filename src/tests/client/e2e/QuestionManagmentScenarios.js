f75541f6ac1be1ee57762fa81b7b17a864e732e9
Igor Baylyak <igor@MacBook-Air-di-Igor.local>
2016-05-11 15:30:04 +0200

    Aggiunto/fix test e2e

--- src/tests/client/e2e/QuestionManagmentScenarios.js
+++ src/tests/client/e2e/QuestionManagmentScenarios.js
@@ -24,22 +24,25 @@ describe('QuestionCreation', function() {
         browser.waitForAngular();
         textareas.sendKeys(
             protractor.Key.CONTROL, "a", protractor.Key.NULL,
-            "<TF F> 2+2=5");
+            "<TF F>\n 2+2=5");
         element(by.buttonText('Submit')).click();
         element(by.css('[ng-click="logout()"]')).click();
     });
     it('docente deve poter modificare una domanda', function() {
 
             element(by.linkText('Gestisci domande')).click();
-            element(by.css('[ng-click="modify(question)"]')).click();
+            element.all(by.css('[ng-click="modify(question)"]')).first().click();
             browser.waitForAngular();
             browser.executeScript('$("div.CodeMirror-wrap > div").removeAttr("style");');
             var textareas = browser.findElement(by.css('[autocorrect="off"]'));
                 element(by.id('tags')).sendKeys("Matematica");
                 browser.waitForAngular();
-                textareas.sendKeys(
-                    protractor.Key.CONTROL, "a", protractor.Key.NULL,
-                    "<TF F> hai mofificato");
+        textareas.sendKeys("");
+        for(var i = 0; i < 50; i++){
+            browser.actions().sendKeys(protractor.Key.DELETE).perform();
+
+        }
+        textareas.sendKeys("<TF F>\n New York è la capitale degli USA? [F]");
         element(by.buttonText('Submit')).click();
         element(by.css('[ng-click="logout()"]')).click();
     });
