2856f0ab36e51b2ed78922454b47be995ada7101
lordnikolai <nicola.decao@studenti.unipd.it>
2016-05-19 15:36:07 +0200

    fix mini bug

--- src/static/js/util/Editor.js
+++ src/static/js/util/Editor.js
@@ -1,11 +1,10 @@
 $(function () {
-    angular.module('EditorModule', []).service('util.Editor', [function () {
-            var self = this;
+    angular.module('EditorModule', []).service('util.Editor', ['util.QML', function (QML) {
             this.editor = function () {
                 return new SimpleMDE({
                     element: document.getElementById('editor'),
                     previewRender: function (plainText) {
-                        var p = self.parse(plainText);
+                        var p = QML.parse(plainText);
                         return p.status ? p.body + p.answerForm : p.message;
                     },
                     toolbar: ['bold', 'italic', '|', 'quote', 'unordered-list', 'ordered-list', '|', 'link', 'image', 'guide', '|', 'preview']
