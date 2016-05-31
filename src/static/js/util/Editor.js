28dad3a81e899212797edc0d45025010a10214b2
lordnikolai <nicola.decao@studenti.unipd.it>
2016-05-31 18:27:09 +0200

    complete text qml

--- src/static/js/util/Editor.js
+++ src/static/js/util/Editor.js
@@ -6,20 +6,7 @@ $(function () {
                     previewRender: function (plainText) {
                         var p = QML.parse(plainText);
                         console.log(p);
-                        
-                        if (p.status) {
-                            if (p.type === 'TF' || p.type === 'MC' || p.type === 'MA') {
-                                return p.body + p.answerForm;
-                            } else if (p.type === 'CT') {
-                                var ret = '';
-                                for (var i = 0; i < p.preview.length; i++) {
-                                    ret += p.body[i] + p.preview[i];
-                                }
-                                return ret + p.body[p.preview.length];
-                            }
-                        } else {
-                            return p.message;
-                        }
+                        return p.status ? p.preview : p.message;
                     },
                     toolbar: ['bold', 'italic', '|', 'quote', 'unordered-list', 'ordered-list', '|', 'link', 'image',
                         {
