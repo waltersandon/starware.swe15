8854df8a4ee93d3d4c82a05d0c73a630636aeb6e
annabonaldo <annabonaldo92@gmail.com>
2016-06-01 11:28:06 +0200

    modifica script

--- src/install.sh
+++ src/install.sh
@@ -1,72 +1,43 @@
 #!/bin/bash
 if [[ $UID != 0 ]]; then
-    echo "Please run this script with sudo:"
-    echo "sudo $0 $*"
-    exit 1
+	echo "Please run this script with sudo:"
+	echo "sudo $0 $*"
+	exit 1
 fi
-if [ "$1" == "-i" ] || [ "$1" == "install" ]; then
-	mongod > /dev/null &
-	mongoPid=""
-	if [ $? -eq 0 ]; then
-		mongoPid=$!
-		if [ $mongoPid != "" ]; then
-			#echo "mongo is running on Pid = $mongoPid"
-			npm install > /dev/null &
-			wait $!
-		else
-			echo "Si è verificato un errore con MongoDB"
-			exit 1
-		fi
-		node ./bootstrap.js
-		wait $!
-		pkill mongod > /dev/null &
-	else
-		echo "Si è verificato un errore con MongoDB"
-		exit 1
-	fi
-elif [ "$1" == "-r" ] || [ "$1" == "reset" ]; then
-	mongod > /dev/null &
-	mongoPid=""
-	if [ $? -eq 0 ]; then
-		mongoPid=$!
-		if [ $mongoPid != "" ]; then
-			#echo "mongo is running on Pid = $mongoPid"
-			sleep 2
-			node ./bootstrap.js &
-			wait $!
 
-		else
-			echo "Si è verificato un errore con MongoDB"
-			exit 1
-		fi
-		pkill mongod > /dev/null &
-	else
-		echo "Si è verificato un errore con MongoDB"
-		exit 1
+export LC_ALL=C
+
+if [ "$1" == "install" ] || [ "$1" == "reset" ] || [ "$1" == "start" ] || [ "$1" == "stop" ]; then
+	pkill node
+	killall node
+	pkill mongod
+	killall mongod
+	
+	if [ "$1" == "install" ]; then
+		npm install
 	fi
-elif [ "$1" == "-s" ] || [ "$1" == "start" ]; then
-	mongod > /dev/null &
-	mongoPid=""
-	serverPid=""
-	if [ $? -eq 0 ]; then
-		mongoPid=$!
-		echo "mongo is running on Pid = $mongoPid"
-		sleep 2
+	
+	if [ "$1" == "install" ] || [ "$1" == "reset" ]; then
+		mongod > /dev/null &
+		echo -ne "MongoDB avviato con successo!\n"
+		node ./bootstrap.js
+		pkill mongod
+		killall mongod
+	elif [ "$1" == "start" ]; then
+		mongod > /dev/null &
+		echo -ne "MongoDB avviato con successo!\n"
 		node ./server.js > /dev/null &
+		echo -ne "Server avviato con successo!\n"
+	elif [ "$1" == "stop" ]; then
+		echo -ne "L'applicazione e' stata fermata con successo!\n"
 	fi
-	if [ $? -eq 0 ]; then
-		serverPid=$!
-		echo "server is running on Pid = $serverPid"
-	fi
-elif [ "$1" == "-q" ] || [ "$1" == "quit" ]; then
-	pkill mongod &
-	pkill node &
-	echo -ne "L'applicazione è stata fermata con successo\n"
 else
 	echo -ne "Lanciare lo script con una delle seguenti opzioni:\n"
-	echo -ne "	-i oppure install :\n		Vengono installate i moduli node necessari all'applicazione\n		viene inizializzato il database con gli utenti, le domande e i questionari di default\n		ATTENZIONE: tutti i dati presenti nel database verranno persi \n\n"
-	echo -ne "	-r oppure reset : \n		Il database viene cancellato e ripristinato con gli utenti, le domande e i questionari di default\n		ATTENZIONE: tutti i dati presenti nel database verranno persi \n\n"
-	echo -ne "	-s oppure start :\n		Viene avviato il database e il server web.\n		 Una volta avviati l'applicazione sarà raggiungibile da browswer all'indirizzo localhost:3000 \n\n"
-	echo -ne "	-q oppure quit :\n		Viene fermato il database e il server web spegnendo completamente l'applicazione \n\n"
+	echo -ne "	install :\n		Vengono installate i moduli node necessari all'applicazione\n		viene inizializzato il database con gli utenti, le domande e i questionari di default\n		ATTENZIONE: tutti i dati presenti nel database verranno persi \n\n"
+	echo -ne "	reset : \n		Il database viene cancellato e ripristinato con gli utenti, le domande e i questionari di default\n		ATTENZIONE: tutti i dati presenti nel database verranno persi \n\n"
+	echo -ne "	start :\n		Viene avviato il database e il server web.\n		 Una volta avviati l'applicazione sara' raggiungibile da browswer all'indirizzo localhost:3000 \n\n"
+	echo -ne "	stop :\n		Viene fermato il database e il server web spegnendo completamente l'applicazione \n\n"
 fi
- 
+
+
+
