9e18a5155578f47c2f2000ea4d2747cbd03581ce
Shoe <tpigarelli@gmail.com>
2016-06-03 15:39:01 +0200

    [Server] Aggiunti test che falliscono per le risposte

---
+++ src/tests/server/unit/service/AnswerServiceTest.js
@@ -0,0 +1,214 @@
+var expect = require('chai').expect;
+var request = require('supertest');
+
+var login = require('../../utils/LoginUtils').login;
+var app = require('../../utils/AppUtils').testApp;
+
+describe('/api/answers', function() {
+
+    describe('GET /api/answers', function() {
+
+        it("permette l'accesso ad un utente non autenticato", function (done) {
+            request(app)
+                .get('/api/answers')
+                .end(function(err, res) {
+                    expect(err).to.not.be.ok;
+                    expect(res).to.have.property('status', 200);
+                    expect(res.body).to.be.instanceof(Array);
+                    expect(res.body.length).to.be.equal(4);
+                    expect(res.body[0]).to.have.property('_id');
+                    expect(res.body[0]).to.have.property('question');
+                    expect(res.body[0]).to.have.property('questionnaire');
+                    expect(res.body[0]).to.have.property('author');
+                    expect(res.body[0]).to.have.property('score');
+                    done();
+                });
+        });
+
+        it("ritorna la lista delle domande all'utente autenticato", function (done) {
+            login(app, {
+                userName: 'tullio.vardanega',
+                password: 'password.tullio.vardanega'
+            }, function(agent) {
+                var req = request(app).get('/api/answers');
+                agent.attachCookies(req);
+                req.end(function(err, res) {
+                    expect(err).to.not.be.ok;
+                    expect(res).to.have.property('status', 200);
+                    expect(res.body).to.be.instanceof(Array);
+                    expect(res.body.length).to.be.equal(4);
+                    expect(res.body[0]).to.have.property('_id');
+                    expect(res.body[0]).to.have.property('question');
+                    expect(res.body[0]).to.have.property('questionnaire');
+                    expect(res.body[0]).to.have.property('author');
+                    expect(res.body[0]).to.have.property('score');
+                    done();
+                });
+            });
+        });
+
+    });
+
+    describe('GET /api/answers/:id', function() {
+
+        it("permette l'accesso ad un utente non autenticato", function (done) {
+            request(app)
+                    .get('/api/answers')
+                    .end(function(err, res) {
+                expect(err).to.not.be.ok;
+                expect(res).to.have.property('status', 200);
+                expect(res.body).to.be.instanceof(Array);
+                expect(res.body.length).to.be.above(1);
+
+                var answer = res.body[0];
+                request(app)
+                        .get('/api/answers/'+answer._id)
+                        .end(function(err, res) {
+                    expect(err).to.not.be.ok;
+                    expect(res).to.have.property('status', 200);
+                    expect(res.body).to.have.property('_id', answer._id);
+                    expect(res.body).to.have.property('question', answer.question);
+                    expect(res.body).to.have.property('questionnaire', answer.questionnaire);
+                    expect(res.body).to.have.property('author', answer.author);
+                    expect(res.body).to.have.property('score', answer.score);
+                });
+                done();
+            });
+        });
+
+        it("ritorna la domanda specificata", function (done) {
+            login(app, {
+                userName: 'tullio.vardanega',
+                password: 'password.tullio.vardanega'
+            }, function(agent) {
+                var req = request(app).get('/api/answers');
+                agent.attachCookies(req);
+                req.end(function(err, res) {
+                    expect(err).to.not.be.ok;
+                    expect(res).to.have.property('status', 200);
+                    expect(res.body).to.be.instanceof(Array);
+                    expect(res.body.length).to.be.above(1);
+
+                    var answer = res.body[0];
+                    var req = request(app).get('/api/answers/'+answer._id);
+                    agent.attachCookies(req);
+                    req.end(function(err, res) {
+                        expect(err).to.not.be.ok;
+                        expect(res).to.have.property('status', 200);
+                        expect(res.body).to.have.property('_id', answer._id);
+                        expect(res.body).to.have.property('question', answer.question);
+                        expect(res.body).to.have.property('questionnaire', answer.questionnaire);
+                        expect(res.body).to.have.property('author', answer.author);
+                        expect(res.body).to.have.property('score', answer.score);
+                    });
+                    done();
+                });
+            });
+        });
+
+    });
+
+    describe('POST /api/answers', function() {
+
+        it("permette l'accesso ad un utente non autenticato", function (done) {
+
+            request(app).get('/api/questionnaires').end(function(err, res) {
+                expect(err).to.not.be.ok;
+                expect(res).to.have.property('status', 200);
+                expect(res.body).to.be.instanceof(Array);
+                expect(res.body.length).to.be.above(1);
+
+                var questionnaire = res.body._id;
+                var question = res.body.questions[0]._id;
+                var newAnswer = {
+                    question: question,
+                    questionnaire: questionnaire,
+                    score: 2
+                };
+                var req = request(app).post('/api/answers').send(newAnswer);
+                agent.attachCookies(req);
+                req.end(function(err, res) {
+                    expect(err).to.not.be.ok;
+                    expect(res).to.have.property('status', 200);
+                    done();
+                });
+
+            });
+
+        });
+
+        it("inserisce la nuova risposta correttamente", function (done) {
+
+            // login
+            login(app, {
+                userName: 'tullio.vardanega',
+                password: 'password.tullio.vardanega'
+            }, function(agent) {
+
+                // get list of questionnaires and select first
+                var req = request(app).get('/api/questionnaires');
+                agent.attachCookies(req);
+                req.end(function(err, res) {
+                    expect(err).to.not.be.ok;
+                    expect(res).to.have.property('status', 200);
+                    expect(res.body).to.be.instanceof(Array);
+                    expect(res.body.length).to.be.above(1);
+
+                    // post a new answer to the first question of the
+                    // questionnaire
+                    var questionnaire = res.body._id;
+                    var question = res.body.questions[0]._id;
+                    var newAnswer = {
+                        question: question,
+                        questionnaire: questionnaire,
+                        score: 2
+                    };
+                    var req = request(app).post('/api/answers').send(newAnswer);
+                    agent.attachCookies(req);
+                    req.end(function(err, res) {
+                        expect(err).to.not.be.ok;
+                        expect(res).to.have.property('status', 200);
+
+                        // get my id
+                        var req = request(app).post('/api/users/me');
+                        agent.attachCookies(req);
+                        req.end(function(err, res) {
+                            expect(err).to.not.be.ok;
+                            expect(res).to.have.property('status', 200);
+                            var author = res.body._id;
+
+                            // search that the new answer exists
+                            var req = request(app).post(
+                                '/api/answers?author=' + author + 
+                                    '&question=' + questions +
+                                    '&questionnaire=' + questionnaire);
+                            agent.attachCookies(req);
+                            req.end(function(err, res) {
+                                expect(err).to.not.be.ok;
+                                expect(res).to.have.property('status', 200);
+                                expect(res.body).to.be.instanceof(Array);
+                                expect(res.body.length).to.be.above(1);
+
+                                var answer = res.body[0];
+                                expect(answer).to.have.property('_id');
+                                expect(answer).to.have.property('question', question);
+                                expect(answer).to.have.property('questionnaire', questionnaire);
+                                expect(answer).to.have.property('score', 2);
+                                expect(answer).to.have.property('author', author);
+
+                                done();
+                            });
+
+                        });
+
+                    });
+
+                });
+
+            });
+
+        });
+
+    });
+
+});
