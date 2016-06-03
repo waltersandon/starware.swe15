db085945099dd0a2242ef2395ad48674d16bb651
lordnikolai <nicola.decao@studenti.unipd.it>
2016-06-03 17:45:46 +0200

    sortable fix

--- src/static/js/controller/student/ExecuteQuestion.js
+++ src/static/js/controller/student/ExecuteQuestion.js
@@ -1,48 +1,48 @@
 $(function () {
     angular.module('app.App').controller('controller.student.ExecuteQuestion', ['$scope', '$sce', '$interpolate', function ($scope, $sce, $interpolate) {
 
-        $scope.ris = {};
+            $scope.ris = {};
 
-        $scope.$watch('currentQuestion', function () {
-            if ($scope.currentQuestion) {
-                $scope.ris.value = $scope.currentQuestion.selectedAnswer;
-                if($scope.currentQuestion.type === "TF" || $scope.currentQuestion.type === "MC" || $scope.currentQuestion.type === "MA" || $scope.currentQuestion.type === "OI"){
-                    $scope.preview = $sce.trustAsHtml($interpolate($scope.currentQuestion.body)($scope));
+            $scope.$watch('currentQuestion', function () {
+                if ($scope.currentQuestion) {
+                    $scope.ris.value = $scope.currentQuestion.selectedAnswer;
+                    if ($scope.currentQuestion.type === "TF" || $scope.currentQuestion.type === "MC" || $scope.currentQuestion.type === "MA") {
+                        $scope.preview = $sce.trustAsHtml($interpolate($scope.currentQuestion.body)($scope));
+                    } else if ($scope.currentQuestion.type === "CT") {
+                        $scope.preview = [];
+                        $scope.currentQuestion.body.forEach(function (b) {
+                            $scope.preview.push($sce.trustAsHtml($interpolate(b)($scope)));
+                        });
+                    } else if ($scope.currentQuestion.type === "OI") {
+                        $scope.preview = $sce.trustAsHtml($interpolate($scope.currentQuestion.body)($scope));
+                        $("#sortable").sortable({
+                            placeholder: "ui-state-highlight",
+                            update: function (event, ui) {
+                                $scope.order = [];
+                                $('#sortable li').each(function (e) {
+                                    $scope.order.push($(this).attr('id'));
+                                });
+                                $scope.changeAnswer();
+                            }
+                        });
+                        $("#sortable").disableSelection();
+                    }
                 }
-                else if($scope.currentQuestion.type === "CT"){
-                    $scope.preview = [];
-                    $scope.currentQuestion.body.forEach(function(b){
-                        $scope.preview.push($sce.trustAsHtml($interpolate(b)($scope)));
-                    });
-                }
-            }
-        });
+            });
 
-        $scope.$watch('ris.value', function () {
-            $scope.changeAnswer();
-        });
-
-        $scope.changeAnswer = function(){
-            if ($scope.currentQuestion) {
-                $scope.currentQuestion.selectedAnswer = $scope.ris.value;
-            }
-        };
-
-        $("#sortable").sortable({
-            placeholder: "ui-state-highlight",
-            update: function (event, ui) {
-                $scope.order = [];
-                $('#sortable li').each(function (e) {
-                    $scope.order.push($(this).attr('id'));
-                });
+            $scope.$watch('ris.value', function () {
                 $scope.changeAnswer();
-            }
-        });
-        $("#sortable").disableSelection();
+            });
 
-        $scope.trust = function(str){
-            return $sce.trustAsHtml(str);
-        };
+            $scope.changeAnswer = function () {
+                if ($scope.currentQuestion) {
+                    $scope.currentQuestion.selectedAnswer = $scope.ris.value;
+                }
+            };
 
-    }]);
+            $scope.trust = function (str) {
+                return $sce.trustAsHtml(str);
+            };
+
+        }]);
 });
