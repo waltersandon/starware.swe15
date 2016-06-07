dae81ae3439d7e02b07e658be73ce7f865aeaef2
waltersandon <walter.sandon79@gmail.com>
2016-06-07 17:49:02 +0200

    inserite le descrizioni nelle classi del package server::validator

--- src/api/validator/TagCheck.js
+++ src/api/validator/TagCheck.js
@@ -1,10 +1,14 @@
 
-/**
+/*
  * Classe che contiene tutte le funzioni di controllo della validità dei campi del model Tag
  * @constructor
  */
 function TagCheck() {}
 
+/*
+ * @details metodo che controlla che il nome dell'argomento non sia vuoto
+ * @param[in]  name il nome dell'argomento da controllare
+ */
 TagCheck.prototype.checkName = function(name){
     return (name.length !== 0);
 };
