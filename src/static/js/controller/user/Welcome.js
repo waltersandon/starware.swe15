25381a72cfdb01320fde884c8fcca6b09271b431
lordnikolai <nicola.decao@studenti.unipd.it>
2016-06-09 12:47:10 +0200

    nuova domanda e chart

--- src/static/js/controller/user/Welcome.js
+++ src/static/js/controller/user/Welcome.js
@@ -4,7 +4,52 @@
  */
 
 $(function () {
-    angular.module('app.App').controller('controller.user.Welcome', ['$scope', function ($scope) {
-            
+    angular.module('app.App').controller('controller.user.Welcome', ['AnswerService', '$scope', '$rootScope', function (AnswerService, $scope, $rootScope) {
+            $scope.retriveStatistics = function () {
+                AnswerService.get(null, null, [$rootScope.me.id], function (res) {
+
+                    var scores = [];
+
+                    res.forEach(function (item) {
+                        scores.push(item.score);
+                    });
+
+                    var vars = new Set(scores);
+                    vars = vars.add(0);
+                    vars = vars.add(1);
+
+                    var vars = Array.from(vars).sort();
+                    var counters = [];
+                    vars.forEach(function () {
+                        counters.push(0);
+                    });
+
+                    var tot = 0;
+                    scores.forEach(function (s) {
+                        var i = vars.indexOf(s);
+                        counters[i]++;
+                        tot++;
+                    });
+
+                    var data = [];
+                    if (tot !== 0) {
+                        for (var i = 0; i < vars.length; i++) {
+                            data.push(counters[i] / tot);
+                        }
+                    }
+
+                    var ctx = document.getElementById("myChart");
+
+                    var Chart = require('js/util/Chart.js/src/chart.js')
+                    var myDoughnutChart = new Chart(ctx, {
+                        type: 'doughnut',
+                        data: data
+                    });
+
+                }, function (res) {
+
+                });
+
+            };
         }]);
 });
--- src/static/js/model/service/AnswerService.js
+++ src/static/js/model/service/AnswerService.js
@@ -16,78 +16,88 @@
 $(function () {
     angular.module('AnswerServiceModule', ['ConfigurationModule']).service('model.service.AnswerService', ['app.Configuration', '$http', function (Configuration, $http) {
 
-        /*!
-         * @details tramite API REST ritorna una lista di questionari sul server
-         *          filtrate in base ai parametri
-         * @param[in]  next          questo parametro rappresenta la callback che
-         *                            il metodo chiama in caso di successo
-         * @param[in]  err           questo parametro rappresenta la callback che
-         *                            il metodo chiama in caso di errore
-         * @param[in]  author        contiene il nome dell'autore del questionario
-         *                            da ricercare
-         * @param[in]  question      contiene la domanda a cui l'utente ha risposto
-         * @param[in]  questionnaire contiene il questionario che l'utente ha
-         *                            eseguito
-         */
-        this.get = function (questionnaire, question, next, err) {
-            $http.get(Configuration.remote + 'api/answers?' +
-                'questionnaires=' + function () {
-                    var a = '';
-                    if (questionnaire instanceof Array)
-                        questionnaire.forEach(function (item) {
-                            a += item + '|';
-                        });
-                    if (a.length >= 1)
-                        a = a.substr(0, a.length - 1);
-                    return a;
-                }() +
-                '&questions=' + function () {
-                    var a = '';
-                    if (question instanceof Array)
-                        question.forEach(function (item) {
-                            a += item + '|';
-                        });
-                    if (a.length >= 1)
-                        a = a.substr(0, a.length - 1);
-                    return a;
-                }()
-            ).then(function success(res) {
+            /*!
+             * @details tramite API REST ritorna una lista di questionari sul server
+             *          filtrate in base ai parametri
+             * @param[in]  next          questo parametro rappresenta la callback che
+             *                            il metodo chiama in caso di successo
+             * @param[in]  err           questo parametro rappresenta la callback che
+             *                            il metodo chiama in caso di errore
+             * @param[in]  author        contiene il nome dell'autore del questionario
+             *                            da ricercare
+             * @param[in]  question      contiene la domanda a cui l'utente ha risposto
+             * @param[in]  questionnaire contiene il questionario che l'utente ha
+             *                            eseguito
+             */
+            this.get = function (questionnaire, question, author, next, err) {
+                $http.get(Configuration.remote + 'api/answers?' +
+                        'questionnaires=' + function () {
+                            var a = '';
+                            if (questionnaire instanceof Array)
+                                questionnaire.forEach(function (item) {
+                                    a += item + '|';
+                                });
+                            if (a.length >= 1)
+                                a = a.substr(0, a.length - 1);
+                            return a;
+                        }() +
+                        '&questions=' + function () {
+                            var a = '';
+                            if (question instanceof Array)
+                                question.forEach(function (item) {
+                                    a += item + '|';
+                                });
+                            if (a.length >= 1)
+                                a = a.substr(0, a.length - 1);
+                            return a;
+                        }() +
+                        'authors=' + function () {
+                            var a = '';
+                            if (author instanceof Array)
+                                author.forEach(function (item) {
+                                    a += item + '|';
+                                });
+                            if (a.length >= 1)
+                                a = a.substr(0, a.length - 1);
+                            return a;
+                        }()
+                        ).then(function success(res) {
 
-                var ret = [];
-                res.data.forEach(function (item) {
-                    ret.push(item);
+                    var ret = [];
+                    res.data.forEach(function (item) {
+                        ret.push(item);
+                    });
+
+                    next(ret);
+                }, function error(res) {
+                    err(res);
                 });
+            };
 
-                next(ret);
-            }, function error(res) {
-                err(res);
-            });
-        };
-
-        /*!
-         * @details tramite API REST crea una nuova risposta ad una domanda sul
-         *          server
-         *
-         * @param[in]  next          questo parametro rappresenta la callback che
-         *                            il metodo chiama in caso di successo
-         * @param[in]  err           questo parametro rappresenta la callback che
-         *                            il metodo chiama in caso di errore
-         * @param[in]  questionnaire contiene il questionario che l'utente ha
-         *                            eseguito
-         * @param[in]  question      contiene la domanda a cui l'utente ha risposto
-         * @param[in]  score         contiene il punteggio che l'utente ha
-         *                            totalizzato sulla singola domanda risposta
-         */
-        this.new = function (questionnaire, question, score, next, err) {
-            $http.post(Configuration.remote + 'api/answers', {
-                'questionnaire': questionnaire.id,
-                'question': question.id,
-                'score': score
-            }).then(function success(res) {
-                next(res.data);
-            }, function error(res) {
-                err(res);
-            });
-        };
-    }]);
+            /*!
+             * @details tramite API REST crea una nuova risposta ad una domanda sul
+             *          server
+             *
+             * @param[in]  next          questo parametro rappresenta la callback che
+             *                            il metodo chiama in caso di successo
+             * @param[in]  err           questo parametro rappresenta la callback che
+             *                            il metodo chiama in caso di errore
+             * @param[in]  questionnaire contiene il questionario che l'utente ha
+             *                            eseguito
+             * @param[in]  question      contiene la domanda a cui l'utente ha risposto
+             * @param[in]  score         contiene il punteggio che l'utente ha
+             *                            totalizzato sulla singola domanda risposta
+             */
+            this.new = function (questionnaire, question, score, next, err) {
+                $http.post(Configuration.remote + 'api/answers', {
+                    'questionnaire': questionnaire.id,
+                    'question': question.id,
+                    'score': score
+                }).then(function success(res) {
+                    next(res.data);
+                }, function error(res) {
+                    err(res);
+                });
+            };
+        }]);
 });
